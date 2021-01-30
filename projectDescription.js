da489b8a09db98ddca6cbcf2143f190d3ee4eaea
joaoaoluiz <joao.fernandes22@fatec.sp.gov.br>
2021-01-29 22:12:11 -0300

    Icon rotation on toggle

--- projectDescription.js
+++ projectDescription.js
@@ -1,17 +1,22 @@
 const expandButtons = document.querySelectorAll(".description__button");
 const descriptions = document.querySelectorAll(".description");
-const toggleableIcons = document.querySelectorAll(".fa-angle-down");
+const toggleableIcons = document.querySelectorAll(".arrow-down");
 const sourceButtons = document.querySelectorAll(".project__codeBtn");
+
+function hideDescription(i) {
+    toggleableIcons.item(i).classList.replace("arrow-up", "arrow-down");
+}
+
+function showDescription(i) {
+    toggleableIcons.item(i).classList.replace("arrow-down", "arrow-up");
+}
 
 expandButtons.forEach((button, i) => {
     button.addEventListener('click', () => {
-        if(descriptions.item(i).classList.toggle("description--shown")){
-            toggleableIcons.item(i).classList.replace("fa-angle-down", "fa-angle-up");
-        }
-        else{
-            toggleableIcons.item(i).classList.replace("fa-angle-up", "fa-angle-down");
-            
-        }
+        if (descriptions.item(i).classList.toggle("description--shown"))
+            showDescription(i);
+        else
+            hideDescription(i);
         sourceButtons.item(i).classList.toggle("project__codeBtn--shown");
     });
     button.addEventListener('mouseover', () => {
